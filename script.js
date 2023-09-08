e16b80ef4f3f62bbdaaae8400f46a5feb17934a3
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-09-08 18:08:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let canvas = document.querySelector("#canvas1");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(0,0,150,100)
+
+// let canvas2 = document.getElementsByClassName("canvas");
+// let ctx2 = canvas2.getContext("2d");
+
+// ctx2.fillStyle = "green";
+// ctx2.fillRect(100, 10, 150, 100);
+
+
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = "yellow";
+// ctx.fillRect(10, 10, 150, 100);
+
